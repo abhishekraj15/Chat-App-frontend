392c6915553b630ab9e21a70fb3862200abe354f
Abhishek Rajput <rabhi0766@gmail.com>
2025-02-17 12:34:01 +0530

    first commit

---
+++ src/Pages/Chatpage.js
@@ -0,0 +1,33 @@
+import { Box } from "@chakra-ui/layout";
+import { useEffect, useState } from "react";
+import Chatbox from "../components/Chatbox";
+import MyChats from "../components/MyChats";
+import SideDrawer from "../components/miscellaneous/SideDrawer";
+import { ChatState } from "../Context/ChatProvider";
+import { useHistory } from "react-router-dom/";
+
+const Chatpage = () => {
+  const history = useHistory();
+  const [fetchAgain, setFetchAgain] = useState(false);
+  const { user } = ChatState();
+
+  useEffect(() => {
+    if (!user) {
+      history.push("/"); // Redirect to home page if user is null
+    }
+  }, [user, history]);
+
+  return (
+    <div style={{ width: "100%" }}>
+      {user && <SideDrawer />}
+      <Box d="flex" justifyContent="space-between" w="100%" h="91.5vh" p="10px">
+        {user && <MyChats fetchAgain={fetchAgain} />}
+        {user && (
+          <Chatbox fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
+        )}
+      </Box>
+    </div>
+  );
+};
+
+export default Chatpage;
